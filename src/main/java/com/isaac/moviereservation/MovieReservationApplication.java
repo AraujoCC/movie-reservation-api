97a61f73c732935c1870091d8905a15d570fb0d5
AraujoCC <pietroisaias1@gmail.com>
2026-04-04 22:52:54 -0300

    Fix: MovieReservationApplication habilitando o @Scheduled no ReservationService

--- src/main/java/com/isaac/moviereservation/MovieReservationApplication.java
+++ src/main/java/com/isaac/moviereservation/MovieReservationApplication.java
@@ -1,13 +1,14 @@
 package com.isaac.moviereservation;
-
+ 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.scheduling.annotation.EnableScheduling;
+ 
 @SpringBootApplication
+@EnableScheduling   // habilita o @Scheduled no ReservationService
 public class MovieReservationApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MovieReservationApplication.class, args);
-	}
-
+ 
+    public static void main(String[] args) {
+        SpringApplication.run(MovieReservationApplication.class, args);
+    }
 }
